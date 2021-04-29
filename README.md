bcd41a4739bad3464c9645a5cffe5fbd58b382cf
Griffin Atkinson <69768345+griffthegrouch@users.noreply.github.com>
2021-04-29 17:02:06 -0400

    v1.0 - currently works as intended
    fixed the problem with checking vertically
    fixed the problem with not saving global variables

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-> this project is intended to be a sudoku solver,
+ this project is intended to be a sudoku solver
+
+    # main flow
+    # 1 define one sudoku problem
+    # 2 save working copy of board
+    # 3 draw board
+    # 4 fill board with unsolved sudoku problem
+    # 5 solve working copy of board
+    # 6 display solved board
+
 > 
-> firstly, the program will be able to store a large array of ints as one sudoku board
+> firstly, the program will be able to store a 2d array of ints as one sudoku board
 > 
 > second, the program can turn that sudoku board into a solved version of it
 > 
--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,23 +1,25 @@
+import time
 import tkinter as tk
 
 count = 0
+
 # defining unsolved board
 unsolved_board = [
-    [5, 3, 0,   0, 7, 0,    0, 0, 0],
-    [6, 0, 0,   1, 9, 5,    0, 0, 0],
-    [0, 9, 8,   0, 0, 0,    0, 6, 0],
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
 
-    [8, 0, 0,   0, 6, 0,    0, 0, 3],
-    [4, 0, 0,   8, 0, 3,    0, 0, 1],
-    [7, 0, 0,   0, 2, 0,    0, 0, 6],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
 
-    [0, 6, 0,   0, 0, 0,    2, 8, 0],
-    [0, 0, 0,   4, 1, 9,    0, 0, 0],
-    [0, 0, 0,   0, 8, 0,    0, 7, 9]
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 0],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
 
 # saving working copy of board to solve on
-board = unsolved_board
+grid = unsolved_board
 
 
 class SudokuDisplay(tk.Frame):
@@ -31,44 +33,14 @@ class SudokuDisplay(tk.Frame):
 
     def create_widgets(self):
         # creates solve and quit buttons
-        self.solve_btn = tk.Button(self, text="Solve\n(click me)", fg="green", command=self.solve)
+        self.solve_btn = tk.Button(self, text="Solve\n(click me)", fg="green", command=solve)
         self.solve_btn.pack(side="top")
 
         self.quit_btn = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
         self.quit_btn.pack(side="top")
 
-    def solve(self):
-        print("starting to solve board...")
-        solve_board()
-        self.fill_board()
-
     def draw_board(self):
         # function draws the sudoku grid
-        global canvas
-
-        colour1 = "#f4fac3"
-        colour2 = "#c5faf0"
-
-        canvas.create_rectangle(50, 50, 200, 200,
-                                outline="#fb0", fill=colour1)
-        canvas.create_rectangle(200, 50, 350, 200,
-                                outline="#fb0", fill=colour2)
-        canvas.create_rectangle(350, 50, 500, 200,
-                                outline="#fb0", fill=colour1)
-
-        canvas.create_rectangle(50, 200, 200, 350,
-                                outline="#fb0", fill=colour2)
-        canvas.create_rectangle(200, 200, 350, 350,
-                                outline="#fb0", fill=colour1)
-        canvas.create_rectangle(350, 200, 500, 350,
-                                outline="#fb0", fill=colour2)
-
-        canvas.create_rectangle(50, 350, 200, 500,
-                                outline="#fb0", fill=colour1)
-        canvas.create_rectangle(200, 350, 350, 500,
-                                outline="#fb0", fill=colour2)
-        canvas.create_rectangle(350, 350, 500, 500,
-                                outline="#fb0", fill=colour1)
 
         # draws vertical lines
         canvas.create_line(0, 50, 0, 500)
@@ -99,60 +71,55 @@ class SudokuDisplay(tk.Frame):
 
         canvas.pack(fill=tk.BOTH, expand=1)
 
-    def fill_board(self):
-        # put all board values into a long array, as they're stored as a 2d array
-        global canvas
-        global board
 
-        values = []
-        for element1 in board:
-            for element2 in element1:
-                values.append(element2)
+def fill_board():
+    # put all board values into a long array, as they're stored as a 2d array
 
-        # offset labels because of the size of each text
-        lblx = 13
-        lbly = 5
-        for i in range(81):
-            if i % 9 == 0:
-                lblx = 13
-                lbly += 50
-            lblx += 50
-            text = values[i]
-            if values[i] == 0:
-                text = None
-            label1 = tk.Label(canvas, text=text, fg="black", font=("Helvetica", 25))
-            label1.place(x=lblx, y=lbly)
+    values = []
+    for element1 in grid:
+        for element2 in element1:
+            values.append(element2)
 
-        canvas.pack(fill=tk.BOTH, expand=1)
+    # offset labels because of the size of each text
+    lblx = 13
+    lbly = 5
+    for i in range(81):
+        if i % 9 == 0:
+            lblx = 13
+            lbly += 50
+        lblx += 50
+        text = values[i]
+        if values[i] == 0:
+            text = None
+        label1 = tk.Label(canvas, text=text, fg="black", font=("Helvetica", 25))
+        label1.place(x=lblx, y=lbly)
+
+    canvas.pack(fill=tk.BOTH, expand=1)
 
 
-def solve_board():
+def solve():
     # solves sudoku board
-    global board
+    global grid
     global count
 
-    count += 1
-    print(count)
-    print("working on it!!")
     for y in range(9):
         for x in range(9):
-            if board[y][x] == 0:
+            if grid[y][x] == 0:
                 for n in range(1, 10):
                     if is_possible(y, x, n):
-                        board[y][x] = n
-                        solve_board()
-
-                        board[y][x] = 0
+                        grid[y][x] = n
+                        solve()
+                        grid[y][x] = 0
+                count += 1
                 return
-    print("phew .. done.")
+    fill_board()
+    print(str(count) + "  phew, done.   ")
+
 
 def is_possible(y, x, n):
-
-    global board
-
     # checks vertically and horizontally if the number exists
-    for i in range(9):
-        if board[y][i] == n or board[x][i] == n:
+    for i in range(0, 9):
+        if grid[y][i] == n or grid[i][x] == n:
             return False
 
     # checks all blocks inside of square if number exists
@@ -160,20 +127,11 @@ def is_possible(y, x, n):
     y0 = (y // 3) * 3
     for i in range(0, 3):
         for j in range(0, 3):
-            if board[y0 + i][x0 + j] == n:
+            if grid[y0 + i][x0 + j] == n:
                 return False
     return True
 
 if __name__ == '__main__':
-    # main flow
-    # 1 define one sudoku problem
-    # 2 save working copy of board
-    # 3 draw board
-    # 4 fill board with unsolved sudoku problem
-    # 5 solve working copy of board
-    # 6 display solved board
-
-
     # creating display instance
     root = tk.Tk()
     app = SudokuDisplay(master=root)
@@ -183,6 +141,6 @@ if __name__ == '__main__':
     app.draw_board()
 
     # filling board with unsolved problem
-    app.fill_board()
+    fill_board()
 
     app.mainloop()
